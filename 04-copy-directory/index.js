c88722362d65caf1f0175e48f9eabf969ffe34a6
artantstar <artantstar@gmail.com>
2021-11-07 18:15:57 +0300

    feat: complete task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+async function copy() {
+  const currentFolder = path.join(__dirname, '');
+  const filesFolder = path.join(__dirname, 'files');
+  const filesCopyFolder = path.join(__dirname, 'files-copy');
+
+  try {
+    const data = await fs.readdir(currentFolder, { withFileTypes: true });
+    const filesCheck = data.some((item) => item.name == 'files');
+    const filesCopyCheck = data.some((item) => item.name == 'files-copy');
+
+    if (filesCopyCheck) await fs.rm(filesCopyFolder, { force: true, maxRetries: 10, recursive: true });
+    if (filesCheck) {
+      await copyFiles(filesFolder, filesCopyFolder);
+      console.log('Файлы успешно скопированы');
+    } else console.log('Папка отсутствует, копирование невозможно');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function copyFiles(filesFolder, filesCopyFolder) {
+  await fs.mkdir(filesCopyFolder);
+  const data = await fs.readdir(filesFolder, { withFileTypes: true });
+
+  for (const item of data) {
+    const currentFiles = path.join(filesFolder, item.name);
+    const copyFiles = path.join(filesCopyFolder, item.name);
+
+    if (item.isFile()) {
+      await fs.copyFile(currentFiles, copyFiles);
+    } else {
+      await copyCurrentDir(currentFiles, copyFiles);
+    }
+  }
+}
+
+copy();
